e6a4edc8a2268ae7a499da294387b4e850f8672b
Mohamed Zahw <mohamedzahw@gmail.com>
2019-03-31 15:23:05 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+app.use((req, res, next) => {
+  res.render('maintainance.hbs')
+});
+
+hbs.registerHelper('currentYear', () => {
+  return new Date().getFullYear()
+});
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    welcomeMessage: 'Hello and welcome!',
+    pageTitle : 'Home page',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle : 'About page',
+  });
+});
+
+app.get('/bad', (request, response) => {
+  response.send({
+    errorMessage: 'Error:unknown!'
+  });
+});
+app.listen(3000, () => {
+  console.log('Server is up and running on port 3000');
+});
