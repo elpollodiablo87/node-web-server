5c7bb9183edd499192db723639599d007e508726
Mohamed Zahw <mohamedzahw@gmail.com>
2019-04-01 12:25:29 +0200

    adding projects page

--- server.js
+++ server.js
@@ -31,6 +31,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (request, response) => {
+  response.render('projects.hbs', {
+    pageTitle : 'Projects page'
+  });
+});
+
 app.get('/bad', (request, response) => {
   response.send({
     errorMessage: 'Error:unknown!'
